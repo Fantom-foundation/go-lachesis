1f55ce93b009efb573197901f9bdb008b2c187d8
devintegral2 <devintegral2@sfxdx.ru>
2020-02-18 17:27:10 +0300

    Tests for all config migration methods

--- cmd/lachesis/config_test.go
+++ cmd/lachesis/config_test.go
@@ -5,6 +5,7 @@ import (
 	"github.com/stretchr/testify/assert"
 	"path/filepath"
 	"testing"
+	"time"
 )
 
 func TestConfigParse(t *testing.T) {
@@ -227,6 +228,281 @@ func TestConfigMigrations(t *testing.T) {
 			sec, err = data.findSection("Lachesis.Emitter")
 			assert.NoError(t, err, "Parent section search no error after delete section in config migrations")
 			assert.NotNil(t, sec, "Parent section exists after delete section in config migrations")
+		})
+	})
+
+	t.Run("Rename param", func(t *testing.T) {
+		source := filepath.Join("testdata", "test_config_wo_version.toml")
+		table, err := parseConfigToTable(source)
+		assert.NoError(t, err, "Parse config to Table")
+		data := NewConfigData(table)
+
+		// Add root section
+		migrations := func(data *ConfigData) *migration.Migration {
+			return migration.Init("lachesis-config-test", "ajIr@Quicuj9").
+				NewNamed("v1-test", func() error {
+					err := data.RenameParam("Validator", "Lachesis.Emitter", "ValidatorAddr")
+					return err
+				})
+		}(data)
+
+		idProd := NewConfigIdProducer(data)
+		migrationManager := migration.NewManager(migrations, idProd)
+		err = migrationManager.Run()
+
+		assert.NoError(t, err, "Config migrations success run")
+
+		newVersion, err := data.GetParamString("Version", "")
+		assert.NoError(t, err, "Get version from config data after config migration")
+		assert.Equal(t, "v1-test", newVersion, "Version right after config migrations")
+
+		paramStr, err := data.GetParamString("ValidatorAddr", "Lachesis.Emitter")
+		assert.NoError(t, err, "Get param data no error after rename param in config migrations")
+		assert.NotEmpty(t, paramStr, "Param data not empty after rename param in config migrations")
+	})
+
+	t.Run("Add param", func(t *testing.T) {
+		t.Run("to root", func(t *testing.T) {
+			source := filepath.Join("testdata", "test_config_wo_version.toml")
+			table, err := parseConfigToTable(source)
+			assert.NoError(t, err, "Parse config to Table")
+			data := NewConfigData(table)
+
+			// Add root section
+			migrations := func(data *ConfigData) *migration.Migration {
+				return migration.Init("lachesis-config-test", "ajIr@Quicuj9").
+					NewNamed("v1-test", func() error {
+						err := data.AddParam("NewParamInRoot", "", "string value")
+						return err
+					})
+			}(data)
+
+			idProd := NewConfigIdProducer(data)
+			migrationManager := migration.NewManager(migrations, idProd)
+			err = migrationManager.Run()
+
+			assert.NoError(t, err, "Config migrations success run")
+
+			newVersion, err := data.GetParamString("Version", "")
+			assert.NoError(t, err, "Get version from config data after config migration")
+			assert.Equal(t, "v1-test", newVersion, "Version right after config migrations")
+
+			paramStr, err := data.GetParamString("NewParamInRoot", "")
+			assert.NoError(t, err, "Get param data no error after add param to root in config migrations")
+			assert.NotEmpty(t, paramStr, "Param data not empty after add param to root in config migrations")
+		})
+		t.Run("to section", func(t *testing.T) {
+			source := filepath.Join("testdata", "test_config_wo_version.toml")
+			table, err := parseConfigToTable(source)
+			assert.NoError(t, err, "Parse config to Table")
+			data := NewConfigData(table)
+
+			// Add root section
+			migrations := func(data *ConfigData) *migration.Migration {
+				return migration.Init("lachesis-config-test", "ajIr@Quicuj9").
+					NewNamed("v1-test", func() error {
+						err := data.AddParam("NewParamInSection", "Lachesis.Emitter", "string value")
+						return err
+					})
+			}(data)
+
+			idProd := NewConfigIdProducer(data)
+			migrationManager := migration.NewManager(migrations, idProd)
+			err = migrationManager.Run()
+
+			assert.NoError(t, err, "Config migrations success run")
+
+			newVersion, err := data.GetParamString("Version", "")
+			assert.NoError(t, err, "Get version from config data after config migration")
+			assert.Equal(t, "v1-test", newVersion, "Version right after config migrations")
+
+			paramStr, err := data.GetParamString("NewParamInSection", "Lachesis.Emitter")
+			assert.NoError(t, err, "Get param data no error after add param in section in config migrations")
+			assert.NotEmpty(t, paramStr, "Param data not empty after add param in section in config migrations")
+		})
+	})
+
+	t.Run("Delete param", func(t *testing.T) {
+		source := filepath.Join("testdata", "test_config_wo_version.toml")
+		table, err := parseConfigToTable(source)
+		assert.NoError(t, err, "Parse config to Table")
+		data := NewConfigData(table)
+
+		// Add root section
+		migrations := func(data *ConfigData) *migration.Migration {
+			return migration.Init("lachesis-config-test", "ajIr@Quicuj9").
+				NewNamed("v1-test", func() error {
+					err := data.DeleteParam("Validator", "Lachesis.Emitter")
+					return err
+				})
+		}(data)
+
+		idProd := NewConfigIdProducer(data)
+		migrationManager := migration.NewManager(migrations, idProd)
+		err = migrationManager.Run()
+
+		assert.NoError(t, err, "Config migrations success run")
+
+		newVersion, err := data.GetParamString("Version", "")
+		assert.NoError(t, err, "Get version from config data after config migration")
+		assert.Equal(t, "v1-test", newVersion, "Version right after config migrations")
+
+		paramStr, err := data.GetParamString("Validator", "Lachesis.Emitter")
+		assert.Error(t, err, "Get param data return error after delete param in config migrations")
+		assert.Empty(t, paramStr, "Param data is empty after delete param in config migrations")
+	})
+
+
+	t.Run("Set param value", func(t *testing.T) {
+		t.Run("String", func(t *testing.T) {
+			source := filepath.Join("testdata", "test_config_wo_version.toml")
+			table, err := parseConfigToTable(source)
+			assert.NoError(t, err, "Parse config to Table")
+			data := NewConfigData(table)
+
+			// Add root section
+			migrations := func(data *ConfigData) *migration.Migration {
+				return migration.Init("lachesis-config-test", "ajIr@Quicuj9").
+					NewNamed("v1-test", func() error {
+						err := data.SetParam("Validator", "Lachesis.Emitter", "test value")
+						return err
+					})
+			}(data)
+
+			idProd := NewConfigIdProducer(data)
+			migrationManager := migration.NewManager(migrations, idProd)
+			err = migrationManager.Run()
+
+			assert.NoError(t, err, "Config migrations success run")
+
+			newVersion, err := data.GetParamString("Version", "")
+			assert.NoError(t, err, "Get version from config data after config migration")
+			assert.Equal(t, "v1-test", newVersion, "Version right after config migrations")
+
+			paramStr, err := data.GetParamString("Validator", "Lachesis.Emitter")
+			assert.NoError(t, err, "Get param data return no error after set param in config migrations")
+			assert.Equal(t, "test value", paramStr, "Param data correct after set param in config migrations")
+		})
+		t.Run("Int", func(t *testing.T) {
+			source := filepath.Join("testdata", "test_config_wo_version.toml")
+			table, err := parseConfigToTable(source)
+			assert.NoError(t, err, "Parse config to Table")
+			data := NewConfigData(table)
+
+			// Add root section
+			migrations := func(data *ConfigData) *migration.Migration {
+				return migration.Init("lachesis-config-test", "ajIr@Quicuj9").
+					NewNamed("v1-test", func() error {
+						err := data.SetParam("MaxTxsFromSender", "Lachesis.Emitter", 1112233)
+						return err
+					})
+			}(data)
+
+			idProd := NewConfigIdProducer(data)
+			migrationManager := migration.NewManager(migrations, idProd)
+			err = migrationManager.Run()
+
+			assert.NoError(t, err, "Config migrations success run")
+
+			newVersion, err := data.GetParamString("Version", "")
+			assert.NoError(t, err, "Get version from config data after config migration")
+			assert.Equal(t, "v1-test", newVersion, "Version right after config migrations")
+
+			paramInt, err := data.GetParamInt("MaxTxsFromSender", "Lachesis.Emitter")
+			assert.NoError(t, err, "Get param data return no error after set int param in config migrations")
+			assert.Equal(t, int64(1112233), paramInt, "Param data correct after set int param in config migrations")
+		})
+		t.Run("Float", func(t *testing.T) {
+			source := filepath.Join("testdata", "test_config_wo_version.toml")
+			table, err := parseConfigToTable(source)
+			assert.NoError(t, err, "Parse config to Table")
+			data := NewConfigData(table)
+
+			// Add root section
+			migrations := func(data *ConfigData) *migration.Migration {
+				return migration.Init("lachesis-config-test", "ajIr@Quicuj9").
+					NewNamed("v1-test", func() error {
+						err := data.SetParam("MaxGasRateGrowthFactor", "Lachesis.Emitter", 1.234)
+						return err
+					})
+			}(data)
+
+			idProd := NewConfigIdProducer(data)
+			migrationManager := migration.NewManager(migrations, idProd)
+			err = migrationManager.Run()
+
+			assert.NoError(t, err, "Config migrations success run")
+
+			newVersion, err := data.GetParamString("Version", "")
+			assert.NoError(t, err, "Get version from config data after config migration")
+			assert.Equal(t, "v1-test", newVersion, "Version right after config migrations")
+
+			paramFloat, err := data.GetParamFloat("MaxGasRateGrowthFactor", "Lachesis.Emitter")
+			assert.NoError(t, err, "Get param data return no error after set float param in config migrations")
+			assert.Equal(t, 1.234, paramFloat, "Param data correct after set float param in config migrations")
+		})
+		t.Run("Bool", func(t *testing.T) {
+			source := filepath.Join("testdata", "test_config_wo_version.toml")
+			table, err := parseConfigToTable(source)
+			assert.NoError(t, err, "Parse config to Table")
+			data := NewConfigData(table)
+
+			// Add root section
+			migrations := func(data *ConfigData) *migration.Migration {
+				return migration.Init("lachesis-config-test", "ajIr@Quicuj9").
+					NewNamed("v1-test", func() error {
+						err := data.SetParam("TxIndex", "Lachesis", false)
+						return err
+					})
+			}(data)
+
+			idProd := NewConfigIdProducer(data)
+			migrationManager := migration.NewManager(migrations, idProd)
+			err = migrationManager.Run()
+
+			assert.NoError(t, err, "Config migrations success run")
+
+			newVersion, err := data.GetParamString("Version", "")
+			assert.NoError(t, err, "Get version from config data after config migration")
+			assert.Equal(t, "v1-test", newVersion, "Version right after config migrations")
+
+			paramBool, err := data.GetParamBool("TxIndex", "Lachesis")
+			assert.NoError(t, err, "Get param data return no error after set bool param in config migrations")
+			assert.False(t, paramBool, "Param data correct after set bool param in config migrations")
+		})
+		t.Run("DateTime", func(t *testing.T) {
+			source := filepath.Join("testdata", "test_config_wo_version.toml")
+			table, err := parseConfigToTable(source)
+			assert.NoError(t, err, "Parse config to Table")
+			data := NewConfigData(table)
+
+			testTime0 := time.Now().UTC().Add(time.Hour)
+			testTime := time.Now().UTC()
+
+			// Add root section
+			migrations := func(data *ConfigData) *migration.Migration {
+				return migration.Init("lachesis-config-test", "ajIr@Quicuj9").
+					NewNamed("v1-test", func() error {
+						err := data.AddParam("TestTime", "Node", testTime0)
+						if err != nil { return err }
+						err = data.SetParam("TestTime", "Node", testTime)
+						return err
+					})
+			}(data)
+
+			idProd := NewConfigIdProducer(data)
+			migrationManager := migration.NewManager(migrations, idProd)
+			err = migrationManager.Run()
+
+			assert.NoError(t, err, "Config migrations success run")
+
+			newVersion, err := data.GetParamString("Version", "")
+			assert.NoError(t, err, "Get version from config data after config migration")
+			assert.Equal(t, "v1-test", newVersion, "Version right after config migrations")
+
+			paramTime, err := data.GetParamTime("TestTime", "Node")
+			assert.NoError(t, err, "Get param data return no error after set datetime param in config migrations")
+			assert.Equal(t, testTime, paramTime, "Param data correct after set datetime param in config migrations")
 		})
 	})
 }
