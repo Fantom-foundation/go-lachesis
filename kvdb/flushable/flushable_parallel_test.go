8f8558eb62be487c994eeef932736175fd0844db
devintegral2 <devintegral2@sfxdx.ru>
2020-05-08 05:17:07 +0400

    Add test FlushableDB iterator: TestFlushableParallelCh()

--- kvdb/flushable/flushable_parallel_test.go
+++ kvdb/flushable/flushable_parallel_test.go
@@ -16,6 +16,7 @@ import (
 )
 
 func TestFlushableParallel(t *testing.T) {
+
 	testDuration := 2 * time.Second
 	testPairsNum := uint64(1000)
 
@@ -32,28 +33,32 @@ func TestFlushableParallel(t *testing.T) {
 
 	flushableDb := Wrap(ldb)
 
-	tableMutable1 := table.New(flushableDb, []byte("1"))
 	tableImmutable := table.New(flushableDb, []byte("2"))
+	tableMutable1 := table.New(flushableDb, []byte("1"))
 	tableMutable2 := table.New(flushableDb, []byte("3"))
 
 	// fill data
 	for i := uint64(0); i < testPairsNum; i++ {
-		_ = tableImmutable.Put(bigendian.Int64ToBytes(i), bigendian.Int64ToBytes(i))
+
 		if i == testPairsNum/2 { // a half of data is flushed, other half isn't
 			_ = flushableDb.Flush()
 		}
+		_ = tableImmutable.Put(bigendian.Int64ToBytes(i), bigendian.Int64ToBytes(i))
 	}
 
 	stopped := false
 
 	wg := sync.WaitGroup{}
 	wg.Add(2)
+
 	go func() {
 		assertar := assert.New(t)
 		for !stopped {
 			// iterate over tableImmutable and check its content
 			it := tableImmutable.NewIterator()
+
 			defer it.Release()
+
 			i := uint64(0)
 			for ; it.Next(); i++ {
 				assertar.Equal(bigendian.Int64ToBytes(i), it.Key(), i)
@@ -73,6 +78,7 @@ func TestFlushableParallel(t *testing.T) {
 			// try to spoil data in tableImmutable by updating other tables
 			_ = tableMutable1.Put(bigendian.Int64ToBytes(r.Uint64()%testPairsNum), bigendian.Int64ToBytes(r.Uint64()))
 			_ = tableMutable2.Put(bigendian.Int64ToBytes(r.Uint64() % testPairsNum)[:7], bigendian.Int64ToBytes(r.Uint64()))
+
 			if r.Int63n(100) == 0 {
 				_ = flushableDb.Flush() // flush with 1% chance
 			}
@@ -83,5 +89,102 @@ func TestFlushableParallel(t *testing.T) {
 
 	time.Sleep(testDuration)
 	stopped = true
+	wg.Wait()
+
+}
+
+func TestFlushableParallelCh(t *testing.T) {
+
+	testDuration := 2 * time.Second
+	testPairsNum := uint64(1000)
+
+	dir, err := ioutil.TempDir("", "test-flushable")
+	if err != nil {
+		panic(fmt.Sprintf("can't create temporary directory %s: %v", dir, err))
+	}
+	disk := leveldb.NewProducer(dir)
+
+	// open raw databases
+	ldb := disk.OpenDb("1")
+	defer ldb.Drop()
+	defer ldb.Close()
+
+	flushableDb := Wrap(ldb)
+
+	tableImmutable := table.New(flushableDb, []byte("2"))
+	tableMutable1 := table.New(flushableDb, []byte("1"))
+	tableMutable2 := table.New(flushableDb, []byte("3"))
+
+	// fill data
+	for i := uint64(0); i < testPairsNum; i++ {
+
+		if i == testPairsNum/2 { // a half of data is flushed, other half isn't
+			_ = flushableDb.Flush()
+		}
+		_ = tableImmutable.Put(bigendian.Int64ToBytes(i), bigendian.Int64ToBytes(i))
+	}
+
+	loop_chan := make(chan bool)
+	wg := sync.WaitGroup{}
+	wg.Add(2)
+
+	go func(ch chan bool) {
+
+		assertar := assert.New(t)
+
+	loop:
+		for {
+			select {
+
+			default:
+
+				// iterate over tableImmutable and check its content
+				it := tableImmutable.NewIterator()
+
+				defer it.Release()
+
+				i := uint64(0)
+				for ; it.Next(); i++ {
+					assertar.Equal(bigendian.Int64ToBytes(i), it.Key(), i)
+					assertar.Equal(bigendian.Int64ToBytes(i), it.Value(), i)
+
+					assertar.NoError(it.Error(), i)
+				}
+				assertar.Equal(testPairsNum, i)
+
+			case _, ok := <-ch:
+				if !ok {
+					break loop
+				}
+			}
+		}
+		wg.Done()
+	}(loop_chan)
+
+	go func(ch chan bool) {
+		r := rand.New(rand.NewSource(0))
+	loop:
+		for {
+
+			select {
+			default:
+				// try to spoil data in tableImmutable by updating other tables
+				_ = tableMutable1.Put(bigendian.Int64ToBytes(r.Uint64()%testPairsNum), bigendian.Int64ToBytes(r.Uint64()))
+				_ = tableMutable2.Put(bigendian.Int64ToBytes(r.Uint64() % testPairsNum)[:7], bigendian.Int64ToBytes(r.Uint64()))
+
+				if r.Int63n(10) == 0 {
+					_ = flushableDb.Flush() // flush with 1% chance
+				}
+			case _, ok := <-ch:
+				if !ok {
+					break loop
+				}
+			}
+		}
+		wg.Done()
+	}(loop_chan)
+
+	time.Sleep(testDuration)
+	close(loop_chan)
 	wg.Wait()
 }
