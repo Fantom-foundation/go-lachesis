dc9233361f4b1139e14789b94d31dce5c70c1c85
devintegral2 <devintegral2@sfxdx.ru>
2020-05-19 19:14:19 +0400

    remove Test_Permutation_determinism_concurency (WeightedPermutation calling from  safe concurrent func)

--- utils/weighted_shuffle_test.go
+++ utils/weighted_shuffle_test.go
@@ -3,7 +3,6 @@ package utils
 import (
 	"crypto/sha256"
 	"github.com/Fantom-foundation/go-lachesis/inter/pos"
-	"sync"
 	"testing"
 
 	"github.com/ethereum/go-ethereum/common"
@@ -68,28 +67,28 @@ func Test_Permutation_determinism(t *testing.T) {
 	assertar.Equal([]int{2, 3}, WeightedPermutation(len(weightsArr)/2, weightsArr, hashOf(common.Hash{}, 4)))
 }
 
-func Test_Permutation_determinism_concurency(t *testing.T) {
-
-	assertar := assert.New(t)
-
-	weights := getTestWeightsIncreasing(10)
-	permutation := WeightedPermutation(len(weights), weights, hashOf(common.Hash{}, 0))
-	wg := sync.WaitGroup{}
-
-	for i := 0; i < 100; i++ {
-		wg.Add(1)
-		var tmpWeights = make([]pos.Stake, len(weights))
-		copy(tmpWeights, weights)
-
-		var tmpPermutation = make([]int, len(permutation))
-		copy(tmpPermutation, permutation)
-
-		go func(w []pos.Stake, perm []int) {
-			defer wg.Done()
-
-			p := WeightedPermutation(len(tmpWeights), tmpWeights, hashOf(common.Hash{}, 0))
-			assertar.Equal(p, tmpPermutation)
-		}(tmpWeights, tmpPermutation)
-	}
-	wg.Wait()
-}
+//func Test_Permutation_determinism_concurency(t *testing.T) {
+//
+//	assertar := assert.New(t)
+//
+//	weights := getTestWeightsIncreasing(10)
+//	permutation := WeightedPermutation(len(weights), weights, hashOf(common.Hash{}, 0))
+//	wg := sync.WaitGroup{}
+//
+//	for i := 0; i < 100; i++ {
+//		wg.Add(1)
+//		var tmpWeights = make([]pos.Stake, len(weights))
+//		copy(tmpWeights, weights)
+//
+//		var tmpPermutation = make([]int, len(permutation))
+//		copy(tmpPermutation, permutation)
+//
+//		go func(w []pos.Stake, perm []int) {
+//			defer wg.Done()
+//
+//			p := WeightedPermutation(len(tmpWeights), tmpWeights, hashOf(common.Hash{}, 0))
+//			assertar.Equal(p, tmpPermutation)
+//		}(tmpWeights, tmpPermutation)
+//	}
+//	wg.Wait()
+//}
