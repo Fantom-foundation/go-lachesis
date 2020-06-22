af1dad43b5a34a33498904eeedd6f04903ff3c0a
a.guzev <aguzev@sfxdx.ru>
2020-06-22 21:45:02 +1000

    enable lockedup functionality

--- app/sfc_index_test.go
+++ app/sfc_index_test.go
@@ -14,49 +14,27 @@ import (
 	"github.com/Fantom-foundation/go-lachesis/utils"
 )
 
-func Test(t *testing.T) {
+func TestSFC(t *testing.T) {
 	env := newTestEnv(3)
 	defer env.Close()
 
 	sfcProxy, err := sfcproxy.NewContract(sfc.ContractAddress, env)
 	require.NoError(t, err)
 
-	t.Run("SFC 1.0.0 (genesis)", func(t *testing.T) {
+	var (
+		sfc1 *sfc110.Contract
+		sfc2 *sfc200.Contract
+	)
+
+	t.Run("Genesis v1.0.0", func(t *testing.T) {
 		// nothing to do
 	})
 
 	t.Run("Some transfers 1", func(t *testing.T) {
-		require := require.New(t)
-
-		balances := make([]*big.Int, 3)
-		for i := range balances {
-			balances[i] = env.State().GetBalance(env.Address(i))
-		}
-
-		const N = 2
-		for i := 0; i < N; i++ {
-			env.ApplyBlock(sameEpoch,
-				env.Tx(0, 1, utils.ToFtm(100)),
-			)
-			env.ApplyBlock(sameEpoch,
-				env.Tx(1, 2, utils.ToFtm(100)),
-			)
-			env.ApplyBlock(sameEpoch,
-				env.Tx(2, 0, utils.ToFtm(100)),
-			)
-		}
-
-		gas := big.NewInt(0).Mul(big.NewInt(int64(N*gasLimit)), env.GasPrice)
-		for i := range balances {
-			require.Equal(
-				big.NewInt(0).Sub(balances[i], gas),
-				env.State().GetBalance(env.Address(i)),
-				fmt.Sprintf("account%d", i),
-			)
-		}
+		cicleTransfers(t, env, 3)
 	})
 
-	t.Run("Upgrade to SFC v1.1.0-rc1", func(t *testing.T) {
+	t.Run("Upgrade to v1.1.0-rc1", func(t *testing.T) {
 		require := require.New(t)
 
 		r := env.ApplyBlock(nextEpoch,
@@ -64,52 +42,25 @@ func Test(t *testing.T) {
 		)
 		newImpl := r[0].ContractAddress
 
-		admin := env.transactor(0)
+		admin := env.Payer(1)
 		tx, err := sfcProxy.ContractTransactor.UpgradeTo(admin, newImpl)
 		require.NoError(err)
 		env.ApplyBlock(sameEpoch, tx)
 
-		impl, err := sfcProxy.Implementation(env.caller())
+		impl, err := sfcProxy.Implementation(env.ReadOnly())
 		require.NoError(err)
 		require.Equal(newImpl, impl, "SFC-proxy: implementation address")
 
-		newSfc, err := sfc110.NewContract(sfc.ContractAddress, env)
+		sfc1, err = sfc110.NewContract(sfc.ContractAddress, env)
 		require.NoError(err)
 
-		epoch, err := newSfc.ContractCaller.CurrentEpoch(env.caller())
+		epoch, err := sfc1.ContractCaller.CurrentEpoch(env.ReadOnly())
 		require.NoError(err)
 		require.Equal(uint64(2), epoch.Uint64(), "current epoch")
 	})
 
 	t.Run("Some transfers 2", func(t *testing.T) {
-		require := require.New(t)
-
-		balances := make([]*big.Int, 3)
-		for i := range balances {
-			balances[i] = env.State().GetBalance(env.Address(i))
-		}
-
-		const N = 2
-		for i := 0; i < N; i++ {
-			env.ApplyBlock(sameEpoch,
-				env.Tx(0, 1, utils.ToFtm(100)),
-			)
-			env.ApplyBlock(sameEpoch,
-				env.Tx(1, 2, utils.ToFtm(100)),
-			)
-			env.ApplyBlock(sameEpoch,
-				env.Tx(2, 0, utils.ToFtm(100)),
-			)
-		}
-
-		gas := big.NewInt(0).Mul(big.NewInt(int64(N*gasLimit)), env.GasPrice)
-		for i := range balances {
-			require.Equal(
-				big.NewInt(0).Sub(balances[i], gas),
-				env.State().GetBalance(env.Address(i)),
-				fmt.Sprintf("account%d", i),
-			)
-		}
+		cicleTransfers(t, env, 3)
 	})
 
 	t.Run("Upgrade to SFC newmodel_exp", func(t *testing.T) {
@@ -120,52 +71,68 @@ func Test(t *testing.T) {
 		)
 		newImpl := r[0].ContractAddress
 
-		admin := env.transactor(0)
+		admin := env.Payer(1)
 		tx, err := sfcProxy.ContractTransactor.UpgradeTo(admin, newImpl)
 		require.NoError(err)
 		env.ApplyBlock(sameEpoch, tx)
 
-		impl, err := sfcProxy.Implementation(env.caller())
+		impl, err := sfcProxy.Implementation(env.ReadOnly())
 		require.NoError(err)
 		require.Equal(newImpl, impl, "SFC-proxy: implementation address")
 
-		newSfc, err := sfc200.NewContract(sfc.ContractAddress, env)
+		sfc2, err = sfc200.NewContract(sfc.ContractAddress, env)
 		require.NoError(err)
 
-		epoch, err := newSfc.ContractCaller.CurrentEpoch(env.caller())
+		epoch, err := sfc2.ContractCaller.CurrentEpoch(env.ReadOnly())
 		require.NoError(err)
 		require.Equal(uint64(3), epoch.Uint64(), "current epoch")
 	})
 
 	t.Run("Some transfers 3", func(t *testing.T) {
-		require := require.New(t)
-
-		balances := make([]*big.Int, 3)
-		for i := range balances {
-			balances[i] = env.State().GetBalance(env.Address(i))
-		}
-
-		const N = 2
-		for i := 0; i < N; i++ {
-			env.ApplyBlock(sameEpoch,
-				env.Tx(0, 1, utils.ToFtm(100)),
-			)
-			env.ApplyBlock(sameEpoch,
-				env.Tx(1, 2, utils.ToFtm(100)),
-			)
-			env.ApplyBlock(sameEpoch,
-				env.Tx(2, 0, utils.ToFtm(100)),
-			)
-		}
-
-		gas := big.NewInt(0).Mul(big.NewInt(int64(N*gasLimit)), env.GasPrice)
-		for i := range balances {
-			require.Equal(
-				big.NewInt(0).Sub(balances[i], gas),
-				env.State().GetBalance(env.Address(i)),
-				fmt.Sprintf("account%d", i),
-			)
-		}
+		cicleTransfers(t, env, 3)
 	})
 
+	t.Run("Enable lockedup functionality", func(t *testing.T) {
+		require := require.New(t)
+
+		tx, err := sfc2.StartLockedUp(env.Payer(1), big.NewInt(4))
+		require.NoError(err)
+		env.ApplyBlock(nextEpoch, tx)
+
+		epoch, err := sfc2.FirstLockedUpEpoch(env.ReadOnly())
+		require.NoError(err)
+		require.Equal(uint64(4), epoch.Uint64())
+
+	})
+
+}
+
+func cicleTransfers(t *testing.T, env *testEnv, count uint64) {
+	require := require.New(t)
+
+	balances := make([]*big.Int, 3)
+	for i := range balances {
+		balances[i] = env.State().GetBalance(env.Address(i + 1))
+	}
+
+	for i := uint64(0); i < count; i++ {
+		env.ApplyBlock(sameEpoch,
+			env.Tx(1, 2, utils.ToFtm(100)),
+		)
+		env.ApplyBlock(sameEpoch,
+			env.Tx(2, 3, utils.ToFtm(100)),
+		)
+		env.ApplyBlock(sameEpoch,
+			env.Tx(3, 1, utils.ToFtm(100)),
+		)
+	}
+
+	gas := big.NewInt(0).Mul(big.NewInt(int64(count*gasLimit)), env.GasPrice)
+	for i := range balances {
+		require.Equal(
+			big.NewInt(0).Sub(balances[i], gas),
+			env.State().GetBalance(env.Address(i+1)),
+			fmt.Sprintf("account%d", i),
+		)
+	}
 }
