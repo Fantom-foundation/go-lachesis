aa2de5b64173a8f244f73770c9ba507cee4ec6b3
alex <rusdev.alex@gmail.com>
2020-09-20 02:20:08 +1000

    readme and comments

--- docker/snapshots/snapshotting.sh
+++ docker/snapshots/snapshotting.sh
@@ -12,36 +12,48 @@ PERIOD_SEC=$(( 60 ))
 
 while true
 do
+    # start the node
     lachesis --nousb &
     PID=$!
 
-    while [ $(( $(date +%s)-$(stat -c %Y files/part.snapshot) )) -lt ${PERIOD_SEC} ]
+    # wait for period
+    while
+	AGE_SEC=$(( $(date +%s)-$(stat -c %Y files/part.snapshot) ))
+	[ ${AGE_SEC} -lt ${PERIOD_SEC} ]
     do
         sleep 5
     done
 
+    # wait for new epoch
     EPOCH_WAS=$(cat files/was.epoch 2>/dev/null || echo 0)
     while
-        EPOCH_NOW=$(lachesis attach --exec='ftm.currentEpoch()')
+        EPOCH_NOW=$(lachesis attach --exec='ftm.currentEpoch()') #'
 	[ ${EPOCH_NOW} -le ${EPOCH_WAS} ]
     do
         sleep 5
     done
     
-    #'
+    # stop the node and export new events snapshot
     kill ${PID} && wait ${PID}
     lachesis export files/part.snapshot ${EPOCH_WAS} $((EPOCH_NOW-1))
 
     if [ -f files/${EPOCH_WAS}.snapshot ]
     then
+	# append prev file to the snapshot
 	cp files/${EPOCH_WAS}.snapshot files/${EPOCH_NOW}.snapshot
 	tail -c +9 files/part.snapshot >> files/${EPOCH_NOW}.snapshot
     else
+	# snapshot file is the first
         cp files/part.snapshot files/${EPOCH_NOW}.snapshot
     fi
     echo ${EPOCH_NOW} > files/was.epoch
     
+    # update link to the latest snapshot file
     sed "s/EPOCH/${EPOCH_NOW}/g" index.html > files/index.html
 
-    find ./files/ -name "*.snapshot" -type f -mtime +1 -exec rm -f {} \;
+    # remove snapshots except the 5 last
+    ls -t1 files/*.snapshot | tail -n +5 | while read F
+    do
+	rm -f ${F}
+    done
 done
