74058f23355746d490fe36798802599243deb9db
Egor Lysenko <uprendis@gmail.com>
2020-10-06 15:30:13 +0700

    set version v0.7.1-rc1-evm1.9.22

--- version/version.go
+++ version/version.go
@@ -7,6 +7,6 @@ import (
 func init() {
 	params.VersionMajor = 0     // Major version component of the current release
 	params.VersionMinor = 7     // Minor version component of the current release
-	params.VersionPatch = 0     // Patch version component of the current release
-	params.VersionMeta = "rc.1" // Version metadata to append to the version string
+	params.VersionPatch = 1     // Patch version component of the current release
+	params.VersionMeta = "rc.1-evm1.9.22" // Version metadata to append to the version string
 }
