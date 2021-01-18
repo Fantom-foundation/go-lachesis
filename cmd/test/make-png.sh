227aef546a7c0105ebe569902bad826b193eb58f
alex <rusdev.alex@gmail.com>
2021-01-18 16:52:50 +1000

    rm empty profile

--- cmd/test/make-png.sh
+++ cmd/test/make-png.sh
@@ -1,5 +1,13 @@
 #!/usr/bin/env bash
 
+# rm empty
+for f in heap_*.out; do
+    if [ ! -s $f ]; then
+	rm -f $f
+    fi
+done
+
+# print first and last profile
 go tool pprof -png $(ls -1 heap_*.out | head -n1)
 go tool pprof -png $(ls -1 heap_*.out | tail -n1)
 
