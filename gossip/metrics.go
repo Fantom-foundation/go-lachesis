887abff1f4f0435230be76b8bb34c3e2221753c6
alex <rusdev.alex@gmail.com>
2021-02-12 23:39:20 +1000

    mv meta.Txs to gossip.Txs

--- gossip/metrics.go
+++ gossip/metrics.go
@@ -2,8 +2,6 @@ package gossip
 
 import (
 	"github.com/ethereum/go-ethereum/metrics"
-
-	"github.com/Fantom-foundation/go-lachesis/cmd/tx-storm/meta"
 )
 
 var (
@@ -12,4 +10,4 @@ var (
 	txTtfMeter         = metrics.NewRegisteredHistogram("tx_ttf", nil, metrics.NewUniformSample(500))
 )
 
-var txLatency = meta.NewTxs()
+var txLatency = NewTxs()
