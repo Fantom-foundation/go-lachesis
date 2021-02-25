cb1119f1da3947b3b17cda53d090e81fb78a203c
Egor Lysenko <uprendis@gmail.com>
2021-02-25 12:26:46 +0700

    version up

--- version/version.go
+++ version/version.go
@@ -8,10 +8,10 @@ import (
 )
 
 func init() {
-	params.VersionMajor = 0     // Major version component of the current release
-	params.VersionMinor = 8     // Minor version component of the current release
-	params.VersionPatch = 1     // Patch version component of the current release
-	params.VersionMeta = "rc.5" // Version metadata to append to the version string
+	params.VersionMajor = 1     // Major version component of the current release
+	params.VersionMinor = 0     // Minor version component of the current release
+	params.VersionPatch = 0     // Patch version component of the current release
+	params.VersionMeta = "rc.1" // Version metadata to append to the version string
 }
 
 func BigToString(b *big.Int) string {
