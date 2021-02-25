1de919d22801d5c0fdd020ec4a292495fb09df3b
Egor Lysenko <uprendis@gmail.com>
2021-02-26 02:15:00 +0700

    version up

--- version/version.go
+++ version/version.go
@@ -8,5 +8,5 @@ func init() {
 	params.VersionMajor = 0     // Major version component of the current release
 	params.VersionMinor = 7     // Minor version component of the current release
 	params.VersionPatch = 0     // Patch version component of the current release
-	params.VersionMeta = "rc.2" // Version metadata to append to the version string
+	params.VersionMeta = "rc.3" // Version metadata to append to the version string
 }
